43b53ebf7b93f8f27619e1b521befadcdafd4ce4
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-10-02 16:37:22 -0500

    Improved the script to generate optimized RK methods, and save generated method in a file readable by HyPar

--- RKOptimize/Initial.m
+++ RKOptimize/Initial.m
@@ -1,8 +1,18 @@
- cd /home/ghosh/Codes/cvx/;
- cvx_setup;
+clear all;
+curr_folder = pwd;
+
+fprintf('Initial script to set up CVX and add RK-opt paths\n');
+cvx_path=input('Enter path to cvx: ', 's');
+rk_opt_path=input('Enter path to RK Optimizer: ', 's');
+
+cd(cvx_path);
+cvx_setup;
  
- cd /home/ghosh/Codes/RK-WENO-Opt/;
- path(path,'/home/ghosh/Codes/RK-opt/polyopt');
- path(path,'/home/ghosh/Codes/RK-opt/RKtools');
- path(path,'/home/ghosh/Codes/RK-opt/RK-coeff-opt');
+cd(curr_folder);
+str=strcat(rk_opt_path,'polyopt');
+path(path,str);
+str=strcat(rk_opt_path,'RKtools');
+path(path,str);
+str=strcat(rk_opt_path,'RK-coeff-opt');
+path(path,str);
  
--- RKOptimize/OptimizedRK.m
+++ RKOptimize/OptimizedRK.m
@@ -1,33 +1,69 @@
 clear all;
-clf;
+close all;
 
 fprintf('Make sure you have run cvx_setup and set the path correctly!\n');
-%path(path,'/home/ghosh/Work/Codes/RK-opt/polyopt');
-%path(path,'/home/ghosh/Work/Codes/RK-opt/RKtools');
-%path(path,'/home/ghosh/Work/Codes/RK-opt/RK-coeff-opt');
 
 filename=input('Enter eigenvalues filename: ','s');
 x = load(filename);
 z = x(:,1) + 1i*x(:,2);
 
 p = input('Enter order of method : ');
-s = input('Enter number of stages: ');
-
+smin = input('Enter minimum number of stages: ');
+smax = input('Enter maximum number of stages: ');
 crit = input('Optimize for accuracy (acc) or SSP coeff (ssp): ','s');
 
-[h,coeff]=opt_poly_bisect(z,s,p,'chebyshev','do_plot',true);
-rk = rk_opt(s,p,'erk',crit,'poly_coeff_ind',((p+1):s), ...
-    'poly_coeff_val',coeff((p+2):s+1),'display','iter');
+style1=['k: ','b: ','r: ','k-.','b-.','r-.'];
+style2=['ko ','bo ','ro ','ks ','bs ','rs '];
+ns1=3;
+ns2=ns1-1;
 
-plot(real(h*z),imag(h*z),'bo');
-hold on;
-plotstabreg_func(coeff,1);
+max_name_size=15;
+legend_str=char(zeros(2*(smax-smin+1),max_name_size));
+count=0;
+figure('units','normalized','position',[.1 .1 .6 .6])
+for s=smin:smax
+    [h,coeff]=opt_poly_bisect(z,s,p,'chebyshev','do_plot',false);
+    rk = rk_opt(s,p,'erk',crit,'poly_coeff_ind',((p+1):s), ...
+        'poly_coeff_val',coeff((p+2):s+1),'display','iter');
+    
+    ss=s-smin+1;
+    plot(real(h*z),imag(h*z),strtrim(style2(ns1*ss-ns2:ns1*ss)), ...
+        'MarkerSize',12);
+    hold on;
+    [xa,ya,R] = StabilityRegion(coeff,1);
+    contour(xa,ya,R,[1 1],strtrim(style1(ns1*ss-ns2:ns1*ss)));
+    hold on;
+    string1=['hl   (',num2str(s,'%1d'),' stages)'];
+    string2=['R(z) (',num2str(s,'%1d'),' stages)'];
+    legend_str(2*ss-1,:)=string1;
+    legend_str(2*ss,:)  =string2;
+    
+    % create a name for this method
+    name = strcat('rk_opt_',num2str(p,'%1d'),'_',num2str(s,'%2d'));
+    fprintf('Writing RK method to file %s.\n',strcat(name,'.inp'));
+    fileID = fopen(strcat(name,'.inp'),'w');
+    fprintf(fileID,'1\n');
+    fprintf(fileID,'begin\n');
+    fprintf(fileID,'\tname         %s\n',name);
+    fprintf(fileID,'\tclass        rk\n');
+    fprintf(fileID,'\tnstages      %d\n',s);
+    fprintf(fileID,'\torder        %d\n',p);
+    fprintf(fileID,'\thmax         %1.16e\n',h);
+    fprintf(fileID,'\tA\n');
+    for ii = 1:size(rk.A,1)
+        fprintf(fileID,'\t\t%1.16E\t',rk.A(ii,:));
+        fprintf(fileID,'\n');
+    end
+    fprintf(fileID,'\tb\n');
+    fprintf(fileID,'\t\t%1.16E\t',rk.b(:));
+    fprintf(fileID,'\n');
+    fprintf(fileID,'end\n');
+    fclose(fileID);
+end
+
+xlabel('Real','FontName','Times','FontSize',15);
+ylabel('Imaginary','FontName','Times','FontSize',15);
+set(gca,'FontName','Times','FontSize',15);
+legend(legend_str(1:2*(smax-smin+1),:),'Location','BestOutside');
+grid on;
 hold off;
-
-fprintf('Maximum CFL: %1.16E\n',h);
-fprintf('A:\n');
-fprintf([repmat('%1.16E\t', 1, size(rk.A, 2)) '\n'], rk.A');
-fprintf('b:\n');
-fprintf([repmat('%1.16E\t', 1, size(rk.b, 2)) '\n'], rk.b');
-fprintf('c:\n');
-fprintf([repmat('%1.16E\t', 1, size(rk.c, 2)) '\n'], rk.c');
