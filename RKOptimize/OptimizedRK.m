3e34a28c38cac93a8946743ee5d11957a3c34671
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-10-06 12:50:47 -0500

    Some more additions to the RK optimizer script to automate the generation of optimized methods

--- RKOptimize/OptimizedRK.m
+++ RKOptimize/OptimizedRK.m
@@ -7,63 +7,83 @@ filename=input('Enter eigenvalues filename: ','s');
 x = load(filename);
 z = x(:,1) + 1i*x(:,2);
 
-p = input('Enter order of method : ');
+p    = input('Enter order of method : ');
 smin = input('Enter minimum number of stages: ');
 smax = input('Enter maximum number of stages: ');
 crit = input('Optimize for accuracy (acc) or SSP coeff (ssp): ','s');
 
-style1=['k: ','b: ','r: ','k-.','b-.','r-.'];
-style2=['ko ','bo ','ro ','ks ','bs ','rs '];
+style1=['k: ','b: ','r: ','k-.','b-.','r-.','k- ','b- ','r- ', ...
+        'k--','b--','r--'];
+style2=['ko ','bo ','ro ','ks ','bs ','rs ','k^ ','b^ ','r^ ', ...
+        'kv ','bv ','rv '];
 ns1=3;
 ns2=ns1-1;
 
-max_name_size=15;
+max_name_size=16;
 legend_str=char(zeros(2*(smax-smin+1),max_name_size));
 count=0;
-figure('units','normalized','position',[.1 .1 .6 .6])
+
+scrsz = get(0,'ScreenSize');
+figID = figure('Position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2]);
 for s=smin:smax
     [h,coeff]=opt_poly_bisect(z,s,p,'chebyshev','do_plot',false);
     rk = rk_opt(s,p,'erk',crit,'poly_coeff_ind',((p+1):s), ...
         'poly_coeff_val',coeff((p+2):s+1),'display','iter');
-    
-    ss=s-smin+1;
-    plot(real(h*z),imag(h*z),strtrim(style2(ns1*ss-ns2:ns1*ss)), ...
-        'MarkerSize',12);
-    hold on;
-    [xa,ya,R] = StabilityRegion(coeff,1);
-    contour(xa,ya,R,[1 1],strtrim(style1(ns1*ss-ns2:ns1*ss)));
-    hold on;
-    string1=['hl   (',num2str(s,'%1d'),' stages)'];
-    string2=['R(z) (',num2str(s,'%1d'),' stages)'];
-    legend_str(2*ss-1,:)=string1;
-    legend_str(2*ss,:)  =string2;
-    
-    % create a name for this method
-    name = strcat('rk_opt_',num2str(p,'%1d'),'_',num2str(s,'%2d'));
-    fprintf('Writing RK method to file %s.\n',strcat(name,'.inp'));
-    fileID = fopen(strcat(name,'.inp'),'w');
-    fprintf(fileID,'1\n');
-    fprintf(fileID,'begin\n');
-    fprintf(fileID,'\tname         %s\n',name);
-    fprintf(fileID,'\tclass        rk\n');
-    fprintf(fileID,'\tnstages      %d\n',s);
-    fprintf(fileID,'\torder        %d\n',p);
-    fprintf(fileID,'\thmax         %1.16e\n',h);
-    fprintf(fileID,'\tA\n');
-    for ii = 1:size(rk.A,1)
-        fprintf(fileID,'\t\t%1.16E\t',rk.A(ii,:));
+    if (~isstruct(rk))
+        fprintf('Unable to find RK method of order %d with %d stages ', ...
+            p,s);
+        fprintf(' (%s optimized).\n',crit);
+    else
+        count = count+1;
+        figure(figID);
+        plot(real(h*z),imag(h*z),strtrim(style2(ns1*count-ns2:ns1*count)), ...
+            'MarkerSize',5);
+        hold on;
+        [xa,ya,R] = StabilityRegion(coeff,1);
+        contour(xa,ya,R,[1 1],strtrim(style1(ns1*count-ns2:ns1*count)));
+        hold on;
+        string1=['hl   (',sprintf('%2d',s),' stages)'];
+        string2=['R(z) (',sprintf('%2d',s),' stages)'];
+        legend_str(2*count-1,:)=string1;
+        legend_str(2*count,:)  =string2;
+        
+        % create a name for this method
+        name = strcat('rk_opt_',sprintf('%1d',p),'_',sprintf('%02d',s));
+        fprintf('Writing RK method to file %s.\n',strcat(name,'.inp'));
+        fileID = fopen(strcat(name,'.inp'),'w');
+        fprintf(fileID,'1\n');
+        fprintf(fileID,'begin\n');
+        fprintf(fileID,'\tname         %s\n',name);
+        fprintf(fileID,'\tclass        rk\n');
+        fprintf(fileID,'\tnstages      %d\n',s);
+        fprintf(fileID,'\torder        %d\n',p);
+        fprintf(fileID,'\thmax         %1.16e\n',h);
+        fprintf(fileID,'\tA\n');
+        for ii = 1:size(rk.A,1)
+            fprintf(fileID,'\t\t%1.16E\t',rk.A(ii,:));
+            fprintf(fileID,'\n');
+        end
+        fprintf(fileID,'\tb\n');
+        fprintf(fileID,'\t\t%1.16E\t',rk.b(:));
         fprintf(fileID,'\n');
+        fprintf(fileID,'end\n');
+        fclose(fileID);
     end
-    fprintf(fileID,'\tb\n');
-    fprintf(fileID,'\t\t%1.16E\t',rk.b(:));
-    fprintf(fileID,'\n');
-    fprintf(fileID,'end\n');
-    fclose(fileID);
 end
 
-xlabel('Real','FontName','Times','FontSize',15);
-ylabel('Imaginary','FontName','Times','FontSize',15);
-set(gca,'FontName','Times','FontSize',15);
-legend(legend_str(1:2*(smax-smin+1),:),'Location','BestOutside');
+figure(figID);
+xlabel('Real','FontName','Times','FontSize',14);
+ylabel('Imaginary','FontName','Times','FontSize',14);
+set(gca,'FontName','Times','FontSize',10);
+legend(legend_str(1:2*count,:),'Location','BestOutside');
 grid on;
 hold off;
+
+% save this figure
+figname = strcat('rk_opt_',sprintf('%1d',p),'_',sprintf('%02d',smin), ...
+    '_',sprintf('%02d',smax));
+print(figID,'-depsc2',strcat(figname,'.tmp.eps'));
+cmd=['eps2eps ',figname,'.tmp.eps',' ',figname,'.eps',' && rm *.tmp.eps'];
+system(cmd);
+saveas(figID,strcat(figname,'.fig'),'fig');
+
